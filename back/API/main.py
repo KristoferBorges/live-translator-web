0c6ecf5e6ae10efa5ec0a2e0a32a02bd2f2c6630
Kristofer Borges <kristofer.souza2027@hotmail.com>
2024-06-14 20:25:58 -0300

    feat(api-audio): A API conseguiu entregar o audio e reproduzir no front-end

--- back/API/main.py
+++ back/API/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from gtts import gTTS
@@ -7,15 +8,11 @@ import speech_recognition as sr
 import os
 
 # Ao iniciar o servidor, o arquivo de audio é criado na pasta assets do front-end
-mp3_path = "front\\src\\assets\\audio\\audiotranslatedText.mp3"
-with open(mp3_path, "wb") as f:
-    f.write(b'\x00')
-
 app = FastAPI()
 
 # Configure CORS
 origins = [
-    "http://localhost:5173",
+    "*",
 ]
 
 app.add_middleware(
@@ -26,23 +23,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class TranslateRequestText(BaseModel):
     prefer: str
     response: str
     text: str
+
 
 class TranslateRequestAudio(BaseModel):
     prefer: str
     response: str
     # Implementar o recebimento do audio ou deixar esplícito o caminho do arquivo de audio.
 
+
 def coletarDadosDeTexto(language1, language2, texto):
     """
     Função responsável por coletar os dados do usuário por via de Texto e gravar um audio em uma pasta para uso do front-end.
     """
-    try: 
+    try:
         translator = Translator()
-        translatedText = translator.translate(texto, src=language1[:2], dest=language2[:2])
+        translatedText = translator.translate(texto,
+                                              src=language1[:2],
+                                              dest=language2[:2])
 
         audio = gTTS(
             text=translatedText.text,
@@ -51,9 +53,10 @@ def coletarDadosDeTexto(language1, language2, texto):
 
         audio.save("front\\src\\assets\\audio\\audiotranslatedText.mp3")
         return translatedText.text
-    
+
     except Exception as e:
         return f"Erro: {e}"
+
 
 def coletarDadosDeAudio(language1, language2, Audio):
     """
@@ -62,10 +65,13 @@ def coletarDadosDeAudio(language1, language2, Audio):
     try:
         r = sr.Recognizer()
 
-        textoColetado = r.recognize_google(audio_data=Audio, language=language1)
+        textoColetado = r.recognize_google(audio_data=Audio,
+                                           language=language1)
 
         translator = Translator()
-        translatedText = translator.translate(textoColetado, src=language1[:2], dest=language2[:2])
+        translatedText = translator.translate(textoColetado,
+                                              src=language1[:2],
+                                              dest=language2[:2])
 
         audio = gTTS(
             text=translatedText.text,
@@ -77,18 +83,32 @@ def coletarDadosDeAudio(language1, language2, Audio):
 
     except Exception as e:
         return f"Erro: {e}"
-    
+
 
 @app.post("/api/translate/texto")
 async def post_translate_text(request_data: TranslateRequestText):
-    translated_text = coletarDadosDeTexto(request_data.prefer, request_data.response, request_data.text)
+    translated_text = coletarDadosDeTexto(request_data.prefer,
+                                          request_data.response,
+                                          request_data.text)
     return {"translated_text": translated_text}
+
 
 @app.post("/api/translate/audio")
 async def post_translate_audio(request_data: TranslateRequestAudio):
-    translated_text = coletarDadosDeAudio(request_data.prefer, request_data.response)
+    translated_text = coletarDadosDeAudio(request_data.prefer,
+                                          request_data.response)
     return {"translated_text": translated_text}
+
+
+@app.get("/api/translate/get-audio")
+async def get_audio():
+    file_path = "audiotranslatedText.mp3"
+
+    return FileResponse(file_path,
+                        media_type='audio/mpeg',
+                        filename='arquivo.mp3')
+
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0")
