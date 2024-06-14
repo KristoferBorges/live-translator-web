b2f5238f97e2522286f8dbd24b2e5134958e32fa
joseLucas-code <78504465+joseLucas-code@users.noreply.github.com>
2024-06-14 17:21:12 -0300

    fix(aside menu mobile): corrigido espaçamentos

--- front/src/components/AsideMenuMobile.jsx
+++ front/src/components/AsideMenuMobile.jsx
@@ -17,10 +17,10 @@ const AsideMenuMobile = () => {
   return (
     <aside className="fixed inset-0 w-full h-full z-10 bg-zinc-950/90">
       <div
-        className="fixed w-64 h-full px-4 z-20 bg-zinc-900 to-right-animation flex flex-col"
+        className="fixed w-64 h-full px-4 z-20 bg-zinc-900 to-right-animation flex flex-col overflow-y-auto scroll-custom"
         ref={asideRef}
       >
-        <header className="mb-10 h-14 flex justify-between items-center px-3 md:p-0">
+        <header className="mb-5 py-4 h-14 flex justify-between items-center px-3 md:p-0 sticky top-0 bg-zinc-900">
           <ButtonIcon onClick={() => setMenuMobile(false)} icon={MenuSVG} />
         </header>
         <LanguageSelect title={'Idioma desejado'} choice="prefer" />
