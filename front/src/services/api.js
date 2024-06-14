672214d6257a80870dbefa9e270233b1046d4319
joseLucas-code <78504465+joseLucas-code@users.noreply.github.com>
2024-06-14 18:50:12 -0300

    fix(api): mudança de link api

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,23 +1,13 @@
-const API_URL = 'http://127.0.0.1:8000/api/translate/';
+const API_URL = 'https://0ab2eafd-a4f7-41ce-804f-87fd5c52d169-00-1e6ldckqhcdem.riker.replit.dev/api/translate/texto';
 
 export const TEXT_POST = (body) => {
   return {
-    url: API_URL + 'texto',
+    url: API_URL',
     options: {
       method: 'POST',
       headers: {
         'Content-Type': 'Application/json',
       },
-      body: JSON.stringify(body),
-    },
-  };
-};
-
-export const AUDIO_POST = (body) => {
-  return {
-    url: API_URL + 'audio',
-    options: {
-      method: 'POST',
       body: JSON.stringify(body),
     },
   };
