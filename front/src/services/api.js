219726f5a8cd189608d4fe7d59196d122cb05433
joseLucas-code <78504465+joseLucas-code@users.noreply.github.com>
2024-06-14 19:11:24 -0300

    fix(api): tirado '

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,8 +1,9 @@
-const API_URL = 'https://0ab2eafd-a4f7-41ce-804f-87fd5c52d169-00-1e6ldckqhcdem.riker.replit.dev/api/translate/texto';
+const API_URL =
+  'https://0ab2eafd-a4f7-41ce-804f-87fd5c52d169-00-1e6ldckqhcdem.riker.replit.dev/api/translate/texto';
 
 export const TEXT_POST = (body) => {
   return {
-    url: API_URL',
+    url: API_URL,
     options: {
       method: 'POST',
       headers: {
