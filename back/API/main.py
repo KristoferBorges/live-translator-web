5b133d834cb0d57069c98e08a1178e723e2b4e8b
Kristofer Borges <kristofer.souza2027@hotmail.com>
2024-06-17 18:53:18 -0300

    feature(API): Inserindo mais dados para a coleta(Navegador, Dispositivo)

--- back/API/main.py
+++ back/API/main.py
@@ -1,13 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from gtts import gTTS
 from googletrans import Translator
 import pandas as pd
-from datetime import datetime
 import openpyxl
 import os
+from datetime import datetime
+import user_agents
 
 app = FastAPI()
 
@@ -34,7 +35,8 @@ class TranslateRequestText(BaseModel):
     text: str
 
 
-def coletarDados(language1, language2, text_enviado, text_traduzido):
+def coletarDados(language1, language2, text_enviado, text_traduzido,
+                 user_agent_str):
     """
     Função responsável por coletar os dados significativos.
     """
@@ -50,15 +52,25 @@ def coletarDados(language1, language2, text_enviado, text_traduzido):
             df = pd.read_excel(file_path)
         else:
             df = pd.DataFrame(columns=[
-                'Idioma de fala',
-                'Idioma de Tradução',
-                'Texto enviado',
-                'Texto traduzido',
-                'Horário acessado',
+                'Idioma de fala', 'Idioma de Tradução', 'Texto enviado',
+                'Texto traduzido', 'Navegador', 'Dispositivo móvel', 
+                'Computador', 'Tablet', 'Horário acessado',
+
             ])
+
+        # Obtém informações do agente do usuário
+        user_agent = user_agents.parse(user_agent_str)
+        navegador = user_agent.browser.family
+        mobile = user_agent.is_mobile
+        computador = user_agent.is_pc
+        tablet = user_agent.is_tablet
 
         # Obtém o horário atual
         horario_acessado = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+
+        # Formatação simples
+        text_enviado = text_enviado.capitalize()
+        text_traduzido = text_traduzido.capitalize()
 
         # Novo dado
         new_data = pd.DataFrame([{
@@ -66,7 +78,11 @@ def coletarDados(language1, language2, text_enviado, text_traduzido):
             "Idioma de Tradução": language2,
             "Texto enviado": text_enviado,
             "Texto traduzido": text_traduzido,
-            "Horário acessado": horario_acessado
+            "Navegador": navegador,
+            "Dispositivo móvel": mobile,
+            "Computador": computador,
+            "Tablet": tablet,
+            "Horário acessado": horario_acessado,
         }])
 
         # Adiciona a nova linha ao DataFrame usando pd.concat
@@ -79,7 +95,7 @@ def coletarDados(language1, language2, text_enviado, text_traduzido):
         print(f"Erro: {error}")
 
 
-def traduzirTexto(language1, language2, texto):
+def traduzirTexto(language1, language2, texto, user_agent_str):
     """
     Função responsável por coletar os dados do usuário por via de Texto e gravar um audio em uma pasta para uso do front-end.
     """
@@ -89,15 +105,17 @@ def traduzirTexto(language1, language2, texto):
                                               src=language1[:2],
                                               dest=language2[:2])
 
-        audio = gTTS(
-            text=translatedText.text,
-            lang=language2,
-        )
+        audio = gTTS(text=translatedText.text, lang=language2[:2])
 
         # Processo de coleta de dados
-        coletarDados(language1, language2, texto, translatedText.text)
+        coletarDados(language1, language2, texto, translatedText.text,
+                     user_agent_str)
 
-        audio.save("audio/audiotranslatedText.mp3")
+        audio_path = "audio/audiotranslatedText.mp3"
+        if not os.path.exists('audio'):
+            os.makedirs('audio')
+        audio.save(audio_path)
+        
         return translatedText.text
 
     except Exception as e:
@@ -105,14 +123,15 @@ def traduzirTexto(language1, language2, texto):
 
 
 @app.post("/api/translate/texto")
-async def post_translate_text(request_data: TranslateRequestText):
+async def post_translate_text(request: Request,
+                              request_data: TranslateRequestText):
     """
     API responsável por realizar a tradução do texto, além de criar um audio com base na tradução.
     """
+    user_agent_str = request.headers.get('user-agent')
     translated_text = traduzirTexto(request_data.prefer, request_data.response,
-                                    request_data.text)
-
-    return {"translated_text": translated_text}
+                                    request_data.text, user_agent_str)
+    return {"translated_text": translated_text.capitalize()}
 
 
 @app.get("/api/translate/get-audio")
@@ -121,7 +140,6 @@ async def get_translate_audio():
     API resposável por coletar o audio gerado e encaminhar para o front-end.
     """
     file_path = "audio/audiotranslatedText.mp3"
-
     return FileResponse(file_path,
                         media_type='audio/mpeg',
                         filename='arquivo.mp3')
