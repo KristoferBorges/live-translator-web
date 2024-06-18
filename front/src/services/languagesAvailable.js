07352fe7c12ed1a7b9a1921b8b4e6f2e57b9efe1
joseLucas-code <78504465+joseLucas-code@users.noreply.github.com>
2024-06-18 14:13:22 -0300

    fix(languagesAvailable): removendo não suportadas

--- front/src/services/languagesAvailable.js
+++ front/src/services/languagesAvailable.js
@@ -1,427 +1,71 @@
 const LanguagesAvailable = [
   {
-    lang: 'af',
-    name: 'Africâner',
-  },
-  {
-    lang: 'sq',
-    name: 'Albanês',
-  },
-  {
-    lang: 'am',
-    name: 'Amárico',
-  },
-  {
-    lang: 'ar',
-    name: 'Árabe',
-  },
-  {
-    lang: 'hy',
-    name: 'Armênio',
-  },
-  {
-    lang: 'az',
-    name: 'Azerbaijano',
-  },
-  {
-    lang: 'eu',
-    name: 'Basco',
-  },
-  {
-    lang: 'be',
-    name: 'Bielorrusso',
-  },
-  {
-    lang: 'bn',
-    name: 'Bengali',
-  },
-  {
-    lang: 'bs',
-    name: 'Bósnio',
-  },
-  {
-    lang: 'bg',
-    name: 'Búlgaro',
-  },
-  {
-    lang: 'ca',
-    name: 'Catalão',
-  },
-  {
-    lang: 'ceb',
-    name: 'Cebuano',
-  },
-  {
-    lang: 'ny',
-    name: 'Chichewa',
-  },
-  {
-    lang: 'zh-cn',
-    name: 'Chinês (Simplificado)',
-  },
-  {
-    lang: 'zh-tw',
-    name: 'Chinês (Tradicional)',
-  },
-  {
-    lang: 'co',
-    name: 'Corso',
-  },
-  {
-    lang: 'hr',
-    name: 'Croata',
-  },
-  {
-    lang: 'cs',
-    name: 'Tcheco',
-  },
-  {
-    lang: 'da',
-    name: 'Dinamarquês',
-  },
-  {
-    lang: 'nl',
-    name: 'Holandês',
-  },
-  {
     lang: 'en',
     name: 'Inglês',
-  },
-  {
-    lang: 'eo',
-    name: 'Esperanto',
-  },
-  {
-    lang: 'et',
-    name: 'Estoniano',
-  },
-  {
-    lang: 'tl',
-    name: 'Filipino',
-  },
-  {
-    lang: 'fi',
-    name: 'Finlandês',
-  },
-  {
-    lang: 'fr',
-    name: 'Francês',
-  },
-  {
-    lang: 'fy',
-    name: 'Frísio',
-  },
-  {
-    lang: 'gl',
-    name: 'Galego',
-  },
-  {
-    lang: 'ka',
-    name: 'Georgiano',
-  },
-  {
-    lang: 'de',
-    name: 'Alemão',
-  },
-  {
-    lang: 'el',
-    name: 'Grego',
-  },
-  {
-    lang: 'gu',
-    name: 'Gujarati',
-  },
-  {
-    lang: 'ht',
-    name: 'Haitiano',
-  },
-  {
-    lang: 'ha',
-    name: 'Hauçá',
-  },
-  {
-    lang: 'haw',
-    name: 'Havaiano',
-  },
-  {
-    lang: 'he',
-    name: 'Hebraico',
-  },
-  {
-    lang: 'hi',
-    name: 'Hindi',
-  },
-  {
-    lang: 'hmn',
-    name: 'Hmong',
-  },
-  {
-    lang: 'hu',
-    name: 'Húngaro',
-  },
-  {
-    lang: 'is',
-    name: 'Islandês',
-  },
-  {
-    lang: 'ig',
-    name: 'Igbo',
-  },
-  {
-    lang: 'id',
-    name: 'Indonésio',
-  },
-  {
-    lang: 'ga',
-    name: 'Irlandês',
-  },
-  {
-    lang: 'it',
-    name: 'Italiano',
-  },
-  {
-    lang: 'ja',
-    name: 'Japonês',
-  },
-  {
-    lang: 'jw',
-    name: 'Javanês',
-  },
-  {
-    lang: 'kn',
-    name: 'Canará',
-  },
-  {
-    lang: 'kk',
-    name: 'Cazaque',
-  },
-  {
-    lang: 'km',
-    name: 'Khmer',
-  },
-  {
-    lang: 'ko',
-    name: 'Coreano',
-  },
-  {
-    lang: 'ku',
-    name: 'Curdo (Kurmanji)',
-  },
-  {
-    lang: 'ky',
-    name: 'Quirguiz',
-  },
-  {
-    lang: 'lo',
-    name: 'Lao',
-  },
-  {
-    lang: 'la',
-    name: 'Latim',
-  },
-  {
-    lang: 'lv',
-    name: 'Letão',
-  },
-  {
-    lang: 'lt',
-    name: 'Lituano',
-  },
-  {
-    lang: 'lb',
-    name: 'Luxemburguês',
-  },
-  {
-    lang: 'mk',
-    name: 'Macedônio',
-  },
-  {
-    lang: 'mg',
-    name: 'Malgaxe',
-  },
-  {
-    lang: 'ms',
-    name: 'Malaio',
-  },
-  {
-    lang: 'ml',
-    name: 'Malaiala',
-  },
-  {
-    lang: 'mt',
-    name: 'Maltês',
-  },
-  {
-    lang: 'mi',
-    name: 'Maori',
-  },
-  {
-    lang: 'mr',
-    name: 'Marati',
-  },
-  {
-    lang: 'mn',
-    name: 'Mongol',
-  },
-  {
-    lang: 'my',
-    name: 'Birmanês',
-  },
-  {
-    lang: 'ne',
-    name: 'Nepalês',
-  },
-  {
-    lang: 'no',
-    name: 'Norueguês',
-  },
-  {
-    lang: 'or',
-    name: 'Oriá',
-  },
-  {
-    lang: 'ps',
-    name: 'Pashto',
-  },
-  {
-    lang: 'fa',
-    name: 'Persa',
-  },
-  {
-    lang: 'pl',
-    name: 'Polonês',
-  },
-  {
-    lang: 'pt',
-    name: 'Português',
-  },
-  {
-    lang: 'pa',
-    name: 'Punjabi',
-  },
-  {
-    lang: 'ro',
-    name: 'Romeno',
-  },
-  {
-    lang: 'ru',
-    name: 'Russo',
-  },
-  {
-    lang: 'sm',
-    name: 'Samoano',
-  },
-  {
-    lang: 'gd',
-    name: 'Gaélico Escocês',
-  },
-  {
-    lang: 'sr',
-    name: 'Sérvio',
-  },
-  {
-    lang: 'st',
-    name: 'Sesoto',
-  },
-  {
-    lang: 'sn',
-    name: 'Shona',
-  },
-  {
-    lang: 'sd',
-    name: 'Sindi',
-  },
-  {
-    lang: 'si',
-    name: 'Cingalês',
-  },
-  {
-    lang: 'sk',
-    name: 'Eslovaco',
-  },
-  {
-    lang: 'sl',
-    name: 'Esloveno',
-  },
-  {
-    lang: 'so',
-    name: 'Somali',
   },
   {
     lang: 'es',
     name: 'Espanhol',
   },
   {
-    lang: 'su',
-    name: 'Sundanês',
+    lang: 'zh-CN',
+    name: 'Chinês (Simplificado)',
   },
   {
-    lang: 'sw',
-    name: 'Suaíli',
+    lang: 'zh-TW',
+    name: 'Chinês (Tradicional)',
   },
   {
-    lang: 'sv',
-    name: 'Sueco',
+    lang: 'hi',
+    name: 'Hindi',
   },
   {
-    lang: 'tg',
-    name: 'Tadjique',
+    lang: 'ar',
+    name: 'Árabe',
   },
   {
-    lang: 'ta',
-    name: 'Tâmil',
+    lang: 'fr',
+    name: 'Francês',
   },
   {
-    lang: 'te',
-    name: 'Telugo',
+    lang: 'de',
+    name: 'Alemão',
   },
   {
-    lang: 'th',
-    name: 'Tailandês',
+    lang: 'ja',
+    name: 'Japonês',
   },
   {
-    lang: 'tr',
-    name: 'Turco',
+    lang: 'ru',
+    name: 'Russo',
   },
   {
-    lang: 'uk',
-    name: 'Ucraniano',
+    lang: 'pt',
+    name: 'Português',
   },
   {
-    lang: 'ur',
-    name: 'Urdu',
+    lang: 'it',
+    name: 'Italiano',
   },
   {
-    lang: 'ug',
-    name: 'Uigur',
-  },
-  {
-    lang: 'uz',
-    name: 'Uzbeque',
+    lang: 'ko',
+    name: 'Coreano',
   },
   {
     lang: 'vi',
     name: 'Vietnamita',
   },
   {
-    lang: 'cy',
-    name: 'Galês',
+    lang: 'fa',
+    name: 'Persa',
   },
   {
-    lang: 'xh',
-    name: 'Xhosa',
+    lang: 'ur',
+    name: 'Urdu',
   },
   {
-    lang: 'yi',
-    name: 'Iídiche',
-  },
-  {
-    lang: 'yo',
-    name: 'Iorubá',
-  },
-  {
-    lang: 'zu',
-    name: 'Zulu',
+    lang: 'tr',
+    name: 'Turco',
   },
 ];
 
