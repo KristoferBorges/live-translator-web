e3a15f73f5b6082bcae0df5f46a5b6f21bf47fe5
joseLucas-code <78504465+joseLucas-code@users.noreply.github.com>
2024-06-18 22:36:16 -0300

    style: campo de texto, mensagem enviada

--- front/src/components/ChatTranslate.jsx
+++ front/src/components/ChatTranslate.jsx
@@ -33,7 +33,7 @@ const ChatTranslate = () => {
           <Fragment key={index}>
             {chat.me && (
               <div
-                className="self-end px-10 py-3 rounded-xl bg-neutral-700 max-w-screen-sm"
+                className="self-end px-4 py-2 rounded-xl bg-neutral-700 max-w-screen-sm"
                 key={chat.me}
               >
                 <p>{chat.me}</p>
